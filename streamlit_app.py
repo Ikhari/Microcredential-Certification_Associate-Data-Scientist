ab0ecf749774fd2e6f891543a49131f1db8ceb81
iqbal khariza <72252909+Ikhari@users.noreply.github.com>
2021-12-13 22:22:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# -- Sheet --
-
 import warnings
 import streamlit as st
 import seaborn as sns
@@ -11,15 +7,11 @@ import pandas as pd
 import plotly.express as px
 
 st.title('Test')
-
-# import library pandas
-# Import library numpy
-# Import library matplotlib dan seaborn untuk visualisasi
 plt.style.use('seaborn')
 # me-non aktifkan peringatan pada python
 warnings.filterwarnings("ignore")
 
-# Panggil file (load file bernama Jumlah-kematian.csv) dan simpan dalam dataframe
+# Panggil file (load file bernama Jumlah-kematian.xlsx) dan simpan dalam dataframe
 st.header('Sample Data Jumlah Kematian Tahun 2017-2019')
 dataset = "https://filedn.com/lePVfyAoiNxFBjMKNqcr2O7/MICROCREDENTIAL/ProjectTugasAkhir/Jumlah-kematian.xlsx"
 data = pd.read_excel(dataset)
@@ -29,13 +21,8 @@ st.header('Sample Data Jumlah Kematian')
 st.write(data.head(5))
 data.corr()
 
-# st.write(data.info())
-
 st.write(data.describe().astype(int))
 
-# melihat jumlah baris dan jumlah kolom (bentuk data) pada data df dengan fungsi .shape
-# st.text('jumlah baris dan jumlah kolom')
-# st.write(data.shape)
 
 st.header('Mengecek Data yang Hilang')
 # cek nilai yang hilang / missing values di dalam data
@@ -53,5 +40,4 @@ st.bar_chart(pd.pivot_table(data=data, index='penyebab', values='jumlah', aggfun
 
 st.header('Pivot Tableaa')
 pnyb = ['ASFIKSIA','BBLR','CAMPAK','DEMAM','DIARE','DIFTERI','GANGGUAN DARAH','GANGGUAN METABOLIK','HIPERTENSI','INFEKSI','KELAINAN','KELAINAN SARAF','LAIN-LAIN','MALARIA','PENDARAHAN','PNEUMONIA','SALURAN CERNA','SEPSIS','TETANUS']
-fig = px.pie(df,values='jumlah', names=pnyb, title='my eyes cryin bruh')
-st.plotly_chart(fig)
+fig = px.pie(df,values='jumlah', names=pnyb, title='Pie chart Jumlah Penyebab Kematian ')
